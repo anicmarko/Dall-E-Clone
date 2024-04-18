dc6bbc6f19100738ddba5544e989915377fd51c0
anicmarko <anic.it36.2022@gmail.com>
2024-04-18 19:39:41 +0200

    (feat.) Generating image from prompt

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,10 +15,39 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {}
+  const generateImage = async () => {
+    if(form.prompt){
+      try {
+        setGeneratingImg(true);
+        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({prompt: form.prompt})
+        });
+
+        const data = await response.json;
+
+        setForm({...form, photo: `data:image/jpeg;base64, ${data.photo}`});
+      } catch (error) {
+        alert(error);
+      } finally{
+        setGeneratingImg(false);
+      }
+
+    } else {
+      alert('Please enter a prompt');
+    }
+  }
   const handleSubmit = () => {}
-  const handleChange = (e) => {}
-  const handleSupriseMe = () => {}
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+  const handleSupriseMe = () => {
+    const randPrompt= getRandomPrompt(form.prompt);
+    setForm({...form, prompt: randPrompt});
+  }
 
   return (
     <section className='max-w-7xl mx-auto'>
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,33 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import {Configuration, OpenAIApi} from 'openai';
+import {OpenAI} from 'openai';
 
 dotenv.config();
-
 const router = express.Router();
+
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+router.post('/', async (req,res) =>{
+
+    try {
+        const {prompt} = req.body;
+        
+        const aiResponse = await openai.images.generate({
+            prompt: prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+
+        res.status(200).json({photo: image});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
+    }
+
+});
+
+
+export default router;
