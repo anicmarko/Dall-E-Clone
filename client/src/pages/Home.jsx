2d9e137d42b61d2d5055fc16f66e9e3569a4d487
Marko Anic <anic.it36.2022@gmail.com>
2025-03-09 20:16:41 +0100

    refactor: URLs for HTTP requests are changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     const fetchPost = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-clone-srv.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type' : 'application/json'
