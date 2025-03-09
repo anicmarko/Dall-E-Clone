b4a126a390e96fdad73bc2d8981979cbed6f2db4
Marko Anic <anic.it36.2022@gmail.com>
2025-03-09 20:05:18 +0100

    fix: 2 responses on '/'

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.use('/api/v1/post',postRoutes);
 app.use('/api/v1/dalle',dalleRoutes);
 
 app.get('/', async (req,res) => {
-    res.send('Hello from DALL-E');
     res.status(200).json({message:'Hello from DALL-E'});
 })
 
